084c2ff995410a269f279dc852d315bae9802f0c
benedict <kiokobenedict@gmail.com>
2021-02-04 00:57:27 +0300

    fixed cors issue to allow for react integration

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,67 +1,84 @@
-import React,  { useState } from 'react'
-import PropTypes from 'prop-types';
-import { LazyLoadImage } from 'react-lazy-load-image-component'
-import { FaUser, FaLock } from 'react-icons/fa'
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import { FaUser, FaLock } from "react-icons/fa";
 
 async function loginUser(credentials) {
-    return fetch('http://localhost:8000/api/auth/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(credentials)
-    })
-      .then(data => data.json())
+  return fetch("http://localhost:8000/api/auth/login/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  }).then((data) => data.json());
 }
 
-export default function Login({ setToken}) {
+export default function Login({ setToken }) {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-    const [username, setUserName] = useState();
-    const [password, setPassword] = useState();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const token = await loginUser({
+      email,
+      password,
+    });
+    setToken(token);
+    console.log(token);
+  };
 
-    const handleSubmit = async e => {
-        e.preventDefault();
-        const token = await loginUser({
-          username,
-          password
-        });
-        setToken(token);
-    }
-
-    return (
-        <div className="w-full h-screen flex items-center justify-center">
-            <form className="fw-full md:w-1/3 bg-white rounded-lg" onSubmit={handleSubmit}>
-                <div className="flex font-bold justify-center mt-6">
-                    <span className="h-20 w-20">
-                        <LazyLoadImage src="../../public/avatar.svg" alt=""/>
-                    </span>
-                </div>
-                <h2 class="text-3xl text-center text-gray-700 mb-4 py-5">Sign In</h2>
-                <div className="px-12 pb-10">
-                    <div className="w-full mb-2">
-                        <div className="flex items-center">
-                            <FaUser className='ml-3 fill-current text-gray-400 text-xs z-10' />
-                            <input type='text' placeholder="Email" className="-mx-6 px-8  w-full border rounded px-3 py-2 text-gray-700 focus:outline-none focus:border-indigo-500" onChange={e => setUserName(e.target.value)} />
-                        </div>
-                    </div>
-                    <div className="w-full mb-2">
-                        <div className="flex items-center">
-                        <FaLock className='ml-3 fill-current text-gray-400 text-xs z-10'/>
-                        <input type='password' placeholder="Password" className="-mx-6 px-8 w-full border rounded px-3 py-2 text-gray-700 focus:outline-none focus:border-indigo-500" onChange={e => setPassword(e.target.value)} />
-
-                        </div>
-                    </div>
-                   
-                    <div className="mt-10">
-                        <button className="w-full py-2 rounded-full bg-green-600 text-gray-100 focus:outline-none focus:shodow-outline hover:bg-indigo-600 shadow-lg" type="submit">Login</button>
-                    </div>
-                </div>
-            </form>
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <form
+        className="fw-full md:w-1/3 bg-white rounded-lg"
+        onSubmit={handleSubmit}
+      >
+        <div className="flex font-bold justify-center mt-6">
+          <span className="h-20 w-20">
+            <LazyLoadImage src="../../public/avatar.svg" alt="" />
+          </span>
         </div>
-           
-    )
+        <h2 className="text-3xl text-center text-gray-700 mb-4 py-5">
+          Sign In
+        </h2>
+        <div className="px-12 pb-10">
+          <div className="w-full mb-2">
+            <div className="flex items-center">
+              <FaUser className="ml-3 fill-current text-gray-400 text-xs z-10" />
+              <input
+                type="text"
+                placeholder="Email"
+                className="-mx-6 px-8  w-full border rounded px-3 py-2 text-gray-700 focus:outline-none focus:border-indigo-500"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="w-full mb-2">
+            <div className="flex items-center">
+              <FaLock className="ml-3 fill-current text-gray-400 text-xs z-10" />
+              <input
+                type="password"
+                placeholder="Password"
+                className="-mx-6 px-8 w-full border rounded px-3 py-2 text-gray-700 focus:outline-none focus:border-indigo-500"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="mt-10">
+            <button
+              className="w-full py-2 rounded-full bg-green-600 text-gray-100 focus:outline-none focus:shodow-outline hover:bg-indigo-600 shadow-lg"
+              type="submit"
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 Login.propTypes = {
-    setToken: PropTypes.func.isRequired
-}
+  setToken: PropTypes.func.isRequired,
+};
--- server/src/app/settings.py
+++ server/src/app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework.authtoken",
+    "corsheaders",
     "drf_yasg",
     "core",
     "user",
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -127,6 +129,13 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+# CORS
+CORS_ALLOWED_ORIGINS = [
+
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
 
 # Static files (CSS, JavaScript, Images)
